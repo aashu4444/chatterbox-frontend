9cb49d0804effd51a0b1cf6798cd1a9c8b915e73
Aashutosh Prajapati <69340582+aashu4444@users.noreply.github.com>
2023-05-10 23:00:41 +0530

    Added http agent to send requests to a non https server

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,14 @@ import { AppContext, AppWrapper } from '@/AppContext';
 import Modal from '@/components/Modal';
 import InfoModal from '@/components/InfoModal';
 import Script from 'next/script';
+import axios from 'axios';
+import http from 'http';
+
+const agent = new http.Agent({
+  rejectUnauthorized: false,
+})
+
+axios.defaults.httpAgent = agent;
 
 export default function App({ Component, pageProps }) {
 
