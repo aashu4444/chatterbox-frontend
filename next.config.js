29e8a6da04868be63de611245d790b77dacf3f3a
Aashutosh Prajapati <69340582+aashu4444@users.noreply.github.com>
2023-05-12 13:16:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'http://54.91.94.172/:path*', // Proxy to Backend
-      },
-    ]
+  async serverMiddleware() {
+    this.express.use(
+      '',
+      createProxyMiddleware({
+        target: 'http://54.91.94.172',
+        changeOrigin: true,
+      })
+    )
   },
   images: {
     remotePatterns: [
